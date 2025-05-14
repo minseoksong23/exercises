2f5ebde3d46984857d6ee98af3028e168d0d09d4
min seok song <minseoksong23@gmail.com>
2025-05-14 19:30:25 +0900

    added search-buggy

--- cs50w/project1/wiki/encyclopedia/views.py
+++ cs50w/project1/wiki/encyclopedia/views.py
@@ -1,11 +1,39 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django import forms
 from . import util
 
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
+
+                
+def subst(word,lst):
+    ls = []
+    len_word=len(word)
+    len_lst=len(lst)
+    for i in range(len_lst):
+        for j in range(len(lst[i])-len_word+1):
+            for k in range(len_word):
+                if word[k] != lst[i][j+k]:
+                    break
+                if k == len_word-1:
+                    ls += [lst[i]]
+    return ls
 
 def index(request):
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            if form.cleaned_data["task"] in util.list_entries:
+                return HttpResponseRedirect(reverse(f"encyclopedia:{form.cleaned_data['task']}"))
+        return render(request, "encyclopedia/index.html", {
+            "form": form,
+            "entries": subst(form.cleaned_data["task"], util.list_entries())
+        })
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "form": NewTaskForm(),
+        " ": entry
     })
 
 def entry(request, name):
