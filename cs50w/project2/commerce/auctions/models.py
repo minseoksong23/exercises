f9432c7b77d678b46fe86a6ee9bbf1ba7377b020
min seok song <minseoksong23@gmail.com>
2025-05-18 19:05:17 +0900

    set up models

--- cs50w/project2/commerce/auctions/models.py
+++ cs50w/project2/commerce/auctions/models.py
@@ -6,18 +6,20 @@ class User(AbstractUser):
     pass
 
 class Listing(models.Model):
-    title=models.CharField(max_length=150)
-    image
-    created #datetime?
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+    starting_bid = models.IntegerField()
+    image = models.URLField(blank=True) # optional
+    category = models.CharField(max_length=20)
+    #category: Fashion, Toys, Electronics, Home, etc
 
-class Bid
-    listing=models.ForeignKey(Listing, on_delete-models.CASCADE, related_name="bid item")
-    bidder #among users?
-    amount
-    placed_at #datetime?
+class Bid(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bid_item")
+    bid = models.IntegerField()
+    bidder = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bidder_for_the_listing") 
 
 class Comment(models.Model):
-    listing
-    author
-    body=
-    posted_at #datetime?
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="item_for_comment")
+    commenter = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter") 
+    body = models.CharField(max_length=300)
+
